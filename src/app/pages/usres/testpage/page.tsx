db398e582c94b8ec74e03f6a0ea7215f7901b02a
Nguyenthai27 <ptejthaii@gmail.com>
2023-12-12 19:31:25 +0700

    components layout

---
+++ src/app/pages/usres/testpage/page.tsx
@@ -0,0 +1,11 @@
+import MainPage from '@/app/components/mainpage';
+import '../assets/css/style.css';
+
+const TestPage = () => {
+    return (
+        <div>
+            <h1> TestPageLearn</h1>
+        </div>
+    )
+}
+export default TestPage
