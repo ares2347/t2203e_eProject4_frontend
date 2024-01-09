fc720a0c98f22fba5b4450488684c2a7fdaa12ac
Hoang Tu <hoangtu.tran@hotmail.com>
2024-01-10 01:41:59 +0700

    update middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      {/* <MainPage /> */}
-      <Link href="/user/travel">Travel</Link>
-      {/* <Travel /> */}
-      {/* <Sucsses/> */}
     </div>
   )
 }
